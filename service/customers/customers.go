9b43da3451e6393bf15bfef5bf0dc3949cc69864
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-28 00:46:03 -0300

    remove

--- service/customers/customers.go
+++ service/customers/customers.go
@@ -1,10 +1,8 @@
 package customers
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/MatthewAraujo/min-ecommerce/repository"
 	"github.com/MatthewAraujo/min-ecommerce/types"
 	"github.com/MatthewAraujo/min-ecommerce/utils"
 	"github.com/gorilla/mux"
@@ -25,23 +23,5 @@ func (h *Handler) RegisterRoutes(router *mux.Router) {
 }
 
 func (h *Handler) getAllCustomers(w http.ResponseWriter, r *http.Request) {
-	customerID := int32(1)
-	orderItems := []repository.OrderItem{
-		{
-			ProductID: 1, // ID do primeiro produto
-			Quantity:  2, // Quantidade solicitada
-		},
-		{
-			ProductID: 2, // ID do segundo produto
-			Quantity:  1, // Quantidade solicitada
-		},
-	}
-	status, err := h.Service.Order(context.Background(), customerID, orderItems)
-	if err != nil {
-		utils.WriteError(w, status, err)
-		return
-	}
-
 	utils.WriteJSON(w, http.StatusOK, "done")
-
 }
--- types/types.go
+++ types/types.go
@@ -1,12 +1,4 @@
 package types
 
-import (
-	"context"
-
-	"github.com/MatthewAraujo/min-ecommerce/repository"
-)
-
 type CostumersService interface {
-	GetAllCustomers() ([]repository.Customer, int, error)
-	Order(context.Context, int32, []repository.OrderItem) (int, error)
 }
