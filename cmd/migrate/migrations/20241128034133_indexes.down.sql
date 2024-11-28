70a45e18ad77f7e7b87a78070ee384fefc08ebf1
MatthewAraujo <matthewaraujo20@gmail.com>
2024-11-28 00:45:07 -0300

    feat: indexex in database

---
+++ cmd/migrate/migrations/20241128034133_indexes.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX idx_order_customer_id;
+DROP INDEX idx_order_order_date;
+DROP INDEX idx_order_status;
+DROP INDEX idx_order_item_order_product;
+DROP INDEX idx_customer_email;
+DROP INDEX idx_product_name;
+DROP INDEX idx_stock_product_id;
---
+++ cmd/migrate/migrations/20241128034133_indexes.up.sql
@@ -0,0 +1,12 @@
+CREATE INDEX idx_order_customer_id ON "orders"(customer_id);
+CREATE INDEX idx_order_order_date ON "orders"(order_date);
+CREATE INDEX idx_order_status ON "order"(status);
+CREATE INDEX idx_order_item_order_product ON "order_items"(order_id, product_id);
+CREATE UNIQUE INDEX idx_customer_email ON "customers"(email);
+CREATE INDEX idx_product_name ON "products"(name);
+CREATE INDEX idx_stock_product_id ON "stocks"(product_id);
+
+
+
+
+
